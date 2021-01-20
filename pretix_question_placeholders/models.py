01242227bd7c8ecb9caca64e6de0e3e4f48b65d1
Tobias Kunze <r@rixx.de>
2021-01-20 13:36:09 +0100

    Add initial comparison logic

--- pretix_question_placeholders/models.py
+++ pretix_question_placeholders/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django_scopes import ScopedManager
 
-from pretix.base.models import Question
+from pretix.base.models import Question, QuestionOption
 
 
 class QuestionPlaceholder(models.Model):
@@ -27,6 +27,31 @@ class QuestionPlaceholder(models.Model):
     )
 
     objects = ScopedManager(organizer="question__event__organizer")
+
+    def render(self, order):
+        from pretix.base.models.orders import QuestionAnswer
+
+        answers = QuestionAnswer.objects.filter(
+            orderposition__order=order, question=self.question
+        )
+        if not answers:
+            if self.use_fallback_when_unanswered:
+                return self.fallback_content
+            return
+
+        if len(answers) > 1:
+            warnings.warn(
+                f"Cannot render email placeholders for multiple answers for {order.code}"
+            )
+            return
+
+        answer = answers[0]
+        for rule in self.rules.all():
+            if rule.matches(answer):
+                return rule.content
+        if self.use_fallback_when_unanswered:
+            return self.fallback_content
+        return ""
 
 
 class PlaceholderRule(models.Model):
@@ -53,3 +78,95 @@ class PlaceholderRule(models.Model):
     condition_operation = models.CharField(
         max_length=4, choices=ComparisonOperation.choices
     )
+
+    @cached_property
+    def question_type(self):
+        return self.placeholder.question.type
+
+    @cached_property
+    def value(self):
+        return self.placeholder.question.clean_answer(self.condition_content)
+
+    @cached_property
+    def allowed_methods(self):
+        global_methods = [self.ComparisonOperation.IS_TRUE]
+
+        comparison_methods = global_methods + [
+            self.ComparisonOperation.EQUALS,
+            self.ComparisonOperation.LESS_THAN,
+            self.ComparisonOperation.LESS_OR_EQUAL_THAN,
+            self.ComparisonOperation.MORE_THAN,
+            self.ComparisonOperation.MORE_OR_EQUAL_THAN,
+        ]
+
+        text_methods = [self.ComparisonOperation.IEQUALS]
+
+        methods = {
+            Question.TYPE_BOOLEAN: global_methods + [self.ComparisonOperation.EQUALS],
+            Question.TYPE_FILE: global_methods,
+            Question.TYPE_DATE: comparison_methods,
+            Question.TYPE_DATETIME: comparison_methods,
+            Question.TYPE_TIME: comparison_methods,
+            Question.TYPE_NUMBER: comparison_methods,
+            Question.TYPE_PHONENUMBER: comparison_methods,
+            Question.TYPE_STRING: comparison_methods + text_methods,
+            Question.TYPE_TEXT: comparison_methods + text_methods,
+            Question.TYPE_COUNTRYCODE: comparison_methods,
+            Question.TYPE_CHOICE: comparison_methods,
+            Question.TYPE_CHOICE_MULTIPLE: comparison_methods,
+        }
+        return set(methods[self.question_type])
+
+    @cached_property
+    def comparison_method(self):
+        methods = {
+            self.ComparisonOperation.EQUALS: self._compare_equals,
+            self.ComparisonOperation.IEQUALS: self._compare_iequals,
+            self.ComparisonOperation.LESS_THAN: self._compare_less_than,
+            self.ComparisonOperation.LESS_OR_EQUAL_THAN: self._compare_less_or_equal_than,
+            self.ComparisonOperation.MORE_THAN: self._compare_more_than,
+            self.ComparisonOperation.MORE_OR_EQUAL_THAN: self._compare_more_or_equal_than,
+            self.ComparisonOperation.IS_TRUE: self._compare_boolean,
+        }
+        return methods[self.comparison_operation]
+
+    def _compare_boolean(self, value):
+        return bool(value)
+
+    def _compare_equals(self, value):
+        return value == self.value
+
+    def _compare_iequals(self, value):
+        return str(value).lower() == str(self.value).lower()
+
+    def _compare_less_than(self, value):
+        return value < self.value
+
+    def _compare_less_or_equal_than(self, value):
+        return value <= self.value
+
+    def _compare_more_than(self, value):
+        return value > self.value
+
+    def _compare_more_or_equal_than(self, value):
+        return value >= self.value
+
+    def matches(self, answer):
+        question = answer.question
+
+        if self.comparison_method not in self.allowed_methods:
+            warnings.warn(f"Forbidden comparison method {self.comparison_method}.")
+            return False
+
+        try:
+            answer_value = answer.question.clean(answer.answer)
+        except Exception as e:
+            warnings.warn(f"Error parsing answer value: {e}")
+            return False
+
+        try:
+            return bool(self.comparison_method(answer_value))
+        except Exception as e:
+            warnings.warn(
+                f"Error when comparing values {self.value} and {answer_value}: {e}"
+            )
---
+++ pretix_question_placeholders/placeholder.py
@@ -0,0 +1,20 @@
+from pretix.base.mail import BaseMailPlaceholder
+
+from .models import QuestionPlaceholder
+
+
+class QuestionMailPlaceholder(BaseMailPlaceholder):
+    def __init__(self, placeholder):
+        self._identifier = f"question_{placeholder.question.id}"
+        self.placeholder = placeholder
+
+    @property
+    def required_context(self):
+        return ["order"]
+
+    @property
+    def identifier(self):
+        return self._identifier
+
+    def render(self, context):
+        return self.placeholder.render(context["order"])
