8df5ba62e26eb622d66cb1a103fd9bebb07cf9bb
Tobias Kunze <r@rixx.de>
2021-01-21 15:14:17 +0100

    Add plugin to sidebar

--- pretix_question_placeholders/signals.py
+++ pretix_question_placeholders/signals.py
@@ -1,5 +1,8 @@
 from django.dispatch import receiver
+from django.urls import resolve, reverse
+from django.utils.translation import gettext_lazy as _
 from pretix.base.signals import register_mail_placeholders
+from pretix.control.signals import nav_event
 
 from .models import QuestionPlaceholder
 from .placeholder import QuestionMailPlaceholder
@@ -14,4 +17,27 @@ def register_mail_question_placeholders(sender, **kwargs):
     return [
         QuestionMailPlaceholder(placeholder)
         for placeholder in get_placeholders_for_event(sender)
+    ]
+
+
+@receiver(nav_event, dispatch_uid="question_placeholders_nav")
+def navbar_info(sender, request, **kwargs):
+    url = resolve(request.path_info)
+    if not request.user.has_event_permission(
+        request.organizer, request.event, "can_change_event_settings"
+    ):
+        return []
+    return [
+        {
+            "label": _("Email placeholders"),
+            "icon": "question-circle-o",
+            "url": reverse(
+                "plugins:pretix_question_placeholders:list",
+                kwargs={
+                    "event": request.event.slug,
+                    "organizer": request.organizer.slug,
+                },
+            ),
+            "active": url.namespace == "plugins:pretix_question_placeholders",
+        }
     ]
