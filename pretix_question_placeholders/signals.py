df24e3a810c52dc9ededa3c642453f4b5dc520bb
Tobias Kunze <r@rixx.de>
2021-01-21 10:53:57 +0100

    Add signal listener

--- pretix_question_placeholders/signals.py
+++ pretix_question_placeholders/signals.py
@@ -1,1 +1,14 @@
-# Register your receivers here
+from django.dispatch import receiver
+
+from pretix.base.signals import register_mail_placeholders
+
+from .models import QuestionPlaceholder
+from .placeholder import QuestionMailPlaceholder
+
+
+@receiver(register_mail_placeholders, dispatch_uid="placeholder_custom")
+def register_mail_question_placeholders(sender, **kwargs):
+    return [
+        QuestionMailPlaceholder(placeholder)
+        for placeholder in QuestionPlaceholder.objects.filter(question__event=sender)
+    ]
